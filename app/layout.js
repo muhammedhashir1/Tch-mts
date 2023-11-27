9de8859f00b5d0064bb6ed5621dda747dead4959
muhammed hashir <hashiraachi818@gmail.com>
2023-11-27 09:04:12 +0530

    updated components

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar/Navbar";
-import Footer from "@/components/footer/footer";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/footer";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Banner from "@/components/banner/Banner";
 import Image from "next/image";
-import Featured from "@/components/featured/featured";
-import Pics from "@/components/pics/pics";
-import Quotes from "@/components/quotes/Quotes";
+import Banner from "./components/banner/Banner";
+import Featured from "./components/featured/featured";
+import Pics from "./components/pics/pics";
+import Quotes from "./components/quotes/Quotes";
 
 export default function Home() {
   return (
